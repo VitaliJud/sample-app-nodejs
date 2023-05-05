1631c71ad89faa17559966aa0807182469d18483
Vitali Judin <65638870+VitaliJud@users.noreply.github.com>
2023-05-05 10:56:15 -0500

    Update auth.ts

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -3,15 +3,15 @@ import { encodePayload, getBCAuth, setSession } from '../../lib/auth';
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     return { status: 200}
-//     try {
-//         // Authenticate the app on install
-//         const session = await getBCAuth(req.query);
-//         const encodedContext = encodePayload(session); // Signed JWT to validate/ prevent tampering
+    try {
+        // Authenticate the app on install
+        const session = await getBCAuth(req.query);
+        const encodedContext = encodePayload(session); // Signed JWT to validate/ prevent tampering
 
-//         await setSession(session);
-//         res.redirect(302, `/?context=${encodedContext}`);
-//     } catch (error) {
-//         const { message, response } = error;
-//         res.status(response?.status || 500).json({ message });
-//     }
+        await setSession(session);
+        res.redirect(302, `/?context=${encodedContext}`);
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
 }
